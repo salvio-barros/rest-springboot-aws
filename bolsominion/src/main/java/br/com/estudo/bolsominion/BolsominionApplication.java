ff7201c133286d56aafd1e8a30dbbf9ad471291b
Salvio de Barros Freire <salvio.freire@dataprev.gov.br>
2021-06-23 16:08:23 -0300

    criando projeto bolsominion

---
+++ bolsominion/src/main/java/br/com/estudo/bolsominion/BolsominionApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudo.bolsominion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolsominionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolsominionApplication.class, args);
+	}
+
+}
---
+++ bolsominion/src/test/java/br/com/estudo/bolsominion/BolsominionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudo.bolsominion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolsominionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
